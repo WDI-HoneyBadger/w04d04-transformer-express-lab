3269d3a13d5ecc087a491513060df72d81876589
Jackie Casper <casper.jaclyn@gmail.com>
2018-12-05 10:52:18 +0300

    transformers lab

---
+++ modules/transformers.js
@@ -0,0 +1,52 @@
+var transformers = [
+  {
+    id: 0,
+    name: 'Optimus Prime',
+    form: 'Freightliner Truck',
+    form_type: 'vehicle',
+    team: 'Autobot',
+    photo: 'http://tfwiki.net/mediawiki/images2/2/2b/Shadowplay2_no_frikkin_way.jpg',
+  },
+  {
+    id: 1,
+    name: 'Megatron',
+    form: 'Gun',
+    form_type: 'object',
+    team: 'Decepticon',
+    photo: 'http://tfwiki.net/mediawiki/images2/thumb/9/92/MTMTE50_megatron_class_dismissed.jpg/562px-MTMTE50_megatron_class_dismissed.jpg',
+  },
+  {
+    id: 2,
+    name: 'Bumblebee',
+    form: 'VW Beetle',
+    form_type: 'vehicle',
+    team: 'Autobot',
+    photo: 'https://i.ytimg.com/vi/kCJFVQTrCac/hqdefault.jpg',
+  },
+  {
+    id: 3,
+    name: 'Soundwave',
+    form: 'Walkman',
+    form_type: 'object',
+    team: 'Decepticon',
+    photo: 'http://tfwiki.net/mediawiki/images2/8/8d/Soundwaves-allofustogether.jpg',
+  },
+  {
+    id: 4,
+    name: 'Starscream',
+    form: 'Fighter Jet',
+    form_type: 'vehicle',
+    team: 'Decepticon',
+    photo: 'http://tfwiki.net/mediawiki/images2/3/33/Ongoing13_sad_starscream_is_sad.jpg',
+  },
+  {
+    id: 5,
+    name: 'Rodimus Prime',
+    form: 'Ford GT',
+    form_type: 'vehicle',
+    team: 'Autobot',
+    photo: 'http://tfwiki.net/mediawiki/images2/thumb/f/f6/SpotlightHotRod-HotRodcvrAexcerpt.jpg/470px-SpotlightHotRod-HotRodcvrAexcerpt.jpg',
+  }
+];
+
+module.exports = transformers
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Express Practice with TRANSFORMERS!
+
+![](https://media.giphy.com/media/vEAPv4pslwGu4/giphy.gif)
+
+We are going to do this lab using **pair programming.** Each of you will work with a parter to complete the application. You will only use *one* computer. One of you, the driver, writes code while the other, the navigator, instructs what to type and reviews each line of code as it is typed in. Half way through, the driver and the navigator will switch roles.
+
+Lets get some more practice with views and controllers by making an app about transformers! You are given a module with some data on transformers. Use express to display this data!
+
+### **The app should have:**
+
+- A `'/'` route displaying a homepage view for the transformers that has a link to `/transformers`
+- A transformers controller with:
+  - A `'/transformers` route listing all of the transformers with their names, images and links to each.
+  - A `'/transformers/:id'` route that shows an individual transformer with all of their data and a link back to `/transformers`
+- Each route should render a html view using mustache!
+
+
+
